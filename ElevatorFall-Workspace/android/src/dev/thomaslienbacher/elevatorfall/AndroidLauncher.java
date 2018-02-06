44c73bf4720d4b1fda49ba7aeaad9cb2515a2bec
thomaslienbacher <lienbacher.tom@gmail.com>
2018-02-06 08:45:15 +0100

    Initial commit

---
+++ ElevatorFall-Workspace/android/src/dev/thomaslienbacher/elevatorfall/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package dev.thomaslienbacher.elevatorfall;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import dev.thomaslienbacher.elevatorfall.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ ElevatorFall-Workspace/desktop/src/dev/thomaslienbacher/elevatorfall/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.thomaslienbacher.elevatorfall.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.thomaslienbacher.elevatorfall.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
